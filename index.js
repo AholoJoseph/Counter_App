5b0c04fb2a445266ebea78d57c61326689d78211
AholoJoseph <ce-jaholo2220@st.umat.edu.gh>
2023-01-28 00:04:31 +0000

    A simple pessenger counter application

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+ let count_entered = document.getElementById("count")
+ console.log(count_entered)
 
-function increment() {
-    console.log("Button clicked")
+ let save_dpl = document.getElementById("save_el")
+ console.log(save_dpl)
 
+ let count = 0
+ function increment() {
+     count += 1
+     count_entered.textContent = count
+ }
+
+
+ function decrement() {
+    count -= 1  
+    count_entered.textContent = count
+ }
+
+function save() {
+    let countString = count + " - "
+    save_dpl.textContent += countString
+    console.log(count)
+    count_entered.textContent = 0
+    count = 0
+    
 }
+
+
+
+
 
 
 
